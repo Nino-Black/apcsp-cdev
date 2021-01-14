00311d00b4213af4b86e912a73c6cee17353189b
Nino Black <blackn3020@my.sduhsd.net>
2021-01-14 08:16:21 +0000

    fixed it

--- alphastrings.c
+++ alphastrings.c
@@ -4,18 +4,29 @@
 int main()
 {
 
- char str1[27];
- char str2[27] = "abcdefghijklmnopqrstuvwxyz";
- for (i = 0; i < 27; i++) {
-  str1[i] -= 97 + i;
+char str1[27];
+ for (i = 0; i < 26; i++) {
+  str1[i] = 'a' + i;
  }
- if (strcmp(str1, str2) == 0)
+
+ char * mstr = str1
+
+ for (i = 0; i < 26; i++) {
+  str1[i] = str1[i] -  32;
+ }
+
+char str2[27] = "abcdefghijklmnopqrstuvwxyz";
+
+ if (strcmp(mstr, str2) == 0)
   printf("These strings are identical\n");
  else
   printf("These strings are different");
- for (i = 0; i < 27; i++) {
-  str1[i] -= 32;
- }
-  printf("Str1: %s\n Str2: %s\n", str1, str2);
-}
 
+char str3[50];
+
+strcat(str3, mstr);
+
+strcat(str3, str2);
+
+printf("Str1: %s\n Str2: %s\n Str3: %s\n", str1, mstr, str3);
+}
