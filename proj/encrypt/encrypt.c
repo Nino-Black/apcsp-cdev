bc5c4d249776022f751d9f51f5369aa430dd43aa
Nino Black <blackn3020@my.sduhsd.net>
2021-01-22 07:30:50 +0000

    update

--- proj/encrypt/encrypt.c
+++ proj/encrypt/encrypt.c
@@ -22,6 +22,13 @@ char shiftChar(char c, int shift, int direction)
   //   shiftChar('c', 3, 1) : 'f'
   //   shiftChar('S', 2, 0) : 'P'
   //   shiftChar('b', 3, 0) : '8'
+  int index = 'a';
+  if (direction == 1) {
+   index = 'a' + c;
+  }
+  if (direction == 0) {
+   index = 'a' - c;
+  }
 }
 
 
--- proj/student.c
+++ proj/student.c
@@ -23,6 +23,15 @@ void createStudent(char* fname, char* lname, int age, int id)
 {
   // createStudent dynamically creates a Student on the heap and adds that
   // student to the student array
+  Student* St = (Student)malloc(sizeof(Student));
+  St -> fname = (char*)malloc((strlen(fname)+1) * sizeof(char));
+  St -> lname = (char*)malloc((strlen(lname)+1) * sizeof(char));
+  strcpy(St -> firstName, fname);
+  strcpy(St -> lastName, lname);
+  St -> age = age;
+  St -> id = id;
+  students[numStudents] = St;
+  numStudents ++;
   //  - the firstName and lastName strings should be dynamically created
   //    based on the size of the fname and lname args
 }
@@ -31,6 +40,9 @@ void createStudent(char* fname, char* lname, int age, int id)
 void deleteStudent(Student* student)
 {
   // free the memory associated with a student including the strings
+  free(St -> fname);
+  free(St -> lname);
+  free(St);
 }
 
 
@@ -38,6 +50,11 @@ void deleteStudents()
 {
   // iterate over the students array deleting every student and setting te pointer
   // values to 0 and adjusting the numStudents to 0
+  for(int i = 0; i < strlen(students); i++) {
+   deleteStudent(students[i]);
+   students[i] = 0;
+  }
+  numStudents = 0;
 }
 
 
@@ -49,12 +66,47 @@ void saveStudents(int key)
   //       tom thumb 15 1234 
   //       james dean 21 2345 
   //       katy jones 18 4532 
+  fp = fopen("studentdata.txt", "w");
+  char buff[256];
+  for (int i = 0; i < strlen(students); i++) {
+   Student* St = students[i];
+   sprintf(buff,"%s %s %d %ld", St -> fname, St -> lname, St -> age, St -> id);
+   if (key != 0) {
+    caesarEncrypt(buff, key);
+   }
+  fprintf(fp, "%s\n", buff);
+  }
+  fclose(fp);
 }
 
 
 void loadStudents(int key)
 {
   // load the students from the data file overwriting all exisiting students in memory
+  char b1[256];
+  char b2[256];
+  char b3[256];
+  char b4[256];
+  if (numStudents > 0) {
+   deleteStudents();
+  }
+  fp = fopen("studentdata.txt", "r");
+  if (fp) {
+   int match = fscanf(fp, "%s %s %s %s", b1, b2, b3, b4);
+   if (match == 4) {
+    if (key != 0) {
+     caesarDecrypt(b1, key);
+     caesarDecrypt(b2, key);
+     caesarDecrypt(b3, key);
+     caesarDecrypt(b4, key);
+     int age;
+     long id;
+     sscanf(b3, "%d", &age);
+     sscanf(b4, "%ld", &id);
+     createStudent(b1, b2, age, id);
+    }
+   }
+  }
 }
 
 
